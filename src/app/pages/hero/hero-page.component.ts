9030a38b2c7c56299c3404c881fa3be6e7fa6d42
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-09-10 19:54:32 -0300

    feat: refactor hero component to use computed properties for description and name

--- src/app/pages/hero/hero-page.component.ts
+++ src/app/pages/hero/hero-page.component.ts
@@ -1,26 +1,36 @@
-import {Component, signal} from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
+import {UpperCasePipe} from '@angular/common';
 
 @Component({
   templateUrl: './hero-page.component.html',
+  imports: [
+    UpperCasePipe
+  ]
 })
 export class HeroComponent {
   name = signal('Ironman');
   age = signal(45);
 
-  getHeroDescription() {
-    return `${this.name()} - ${this.age()}`;
-  }
+  // getHeroDescription() {
+  //   return `${this.name()} - ${this.age()}`;
+  // }
 
-  changeHero(){
+  heroDescription = computed(() => {
+    return `${this.name()} - ${this.age()}`;
+  });
+
+  capitalizedName = computed(() => this.name().toUpperCase());
+
+  changeHero() {
     this.name.set('Spiderman');
     this.age.set(22);
   }
 
-  changeAge(){
+  changeAge() {
     this.age.set(60);
   }
 
-  resetForm(){
+  resetForm() {
     this.name.set('Ironman');
     this.age.set(45);
   }
