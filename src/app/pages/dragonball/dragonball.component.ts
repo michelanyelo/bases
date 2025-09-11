d8c6742a4bcd90045d171d9bb32525b5d4523c28
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-09-11 19:47:14 -0300

    feat: add FOR directive for Dragon Ball component

--- src/app/pages/dragonball/dragonball.component.ts
+++ src/app/pages/dragonball/dragonball.component.ts
@@ -1,8 +1,18 @@
-import {Component} from '@angular/core';
+import {Component, signal} from '@angular/core';
+
+interface Character {
+  id: number;
+  name: string;
+  powerLevel: number;
+}
 
 @Component({
   templateUrl: './dragonball.component.html'
 })
 export class DragonballComponent {
-
+  characters = signal<Character[]>([
+    {id: 1, name: 'Goku', powerLevel: 9001},
+    {id: 2, name: 'Vegeta', powerLevel: 8500},
+    {id: 3, name: 'Gohan', powerLevel: 7000}
+  ]);
 }
