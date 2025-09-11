fc381a490a9a1882ec2e16806b2b1e4c18d2071c
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-09-11 19:29:24 -0300

    feat: add Dragon Ball component and routing to app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
-import { Routes } from '@angular/router';
-import { CounterComponent } from './pages/counter/counter.component';
+import {Routes} from '@angular/router';
+import {CounterComponent} from './pages/counter/counter.component';
 import {HeroComponent} from './pages/hero/hero-page.component';
+import {DragonballComponent} from './pages/dragonball/dragonball.component';
 
 export const routes: Routes = [
   {
@@ -10,6 +11,10 @@ export const routes: Routes = [
   {
     path: 'hero',
     component: HeroComponent,
+  },
+  {
+    path: 'dragonball',
+    component: DragonballComponent,
   },
   {
     path: '**',
