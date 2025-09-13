44154b43e5df4abe665f62bc2cdb6b09624d4e50
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-09-13 18:25:38 -0300

    feat: enhance Dragon Ball component with dynamic badge classes for character power levels

--- src/app/pages/dragonball/dragonball.component.ts
+++ src/app/pages/dragonball/dragonball.component.ts
@@ -1,4 +1,5 @@
-import {Component, signal} from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
+import {NgClass} from '@angular/common';
 
 interface Character {
   id: number;
@@ -7,6 +8,9 @@ interface Character {
 }
 
 @Component({
+  imports: [
+    NgClass
+  ],
   templateUrl: './dragonball.component.html'
 })
 export class DragonballComponent {
@@ -15,4 +19,25 @@ export class DragonballComponent {
     {id: 2, name: 'Vegeta', powerLevel: 8500},
     {id: 3, name: 'Gohan', powerLevel: 7000}
   ]);
+
+  // con computed + css en el componente html
+  // powerClass = computed(() => {
+  //   return {
+  //     'high-power': this.characters().some(c => c.powerLevel > 9000),
+  //     'medium-power': this.characters().some(c => c.powerLevel > 8000 && c.powerLevel <= 9000),
+  //     'low-power': this.characters().every(c => c.powerLevel <= 8000)
+  //   }
+  // });
+
+  // con función + bootstrap
+  getBadgeClass(character: Character) {
+    if (character.powerLevel > 9000) {
+      return 'bg-danger';   // rojo
+    } else if (character.powerLevel > 8000) {
+      return 'bg-warning';  // amarillo/naranja
+    } else {
+      return 'bg-success';  // verde
+    }
+  }
+
 }
