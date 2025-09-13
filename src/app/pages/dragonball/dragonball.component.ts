d4d52688f782492b7c084307e6bb8cccabb164d2
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-09-13 19:59:27 -0300

    feat: implement character addition functionality with input validation

--- src/app/pages/dragonball/dragonball.component.ts
+++ src/app/pages/dragonball/dragonball.component.ts
@@ -14,6 +14,9 @@ interface Character {
   templateUrl: './dragonball.component.html'
 })
 export class DragonballComponent {
+  name = signal('');
+  powerLevel = signal(0);
+
   characters = signal<Character[]>([
     {id: 1, name: 'Goku', powerLevel: 9001},
     {id: 2, name: 'Vegeta', powerLevel: 8500},
@@ -27,15 +30,6 @@ export class DragonballComponent {
     {id: 10, name: 'Yamcha', powerLevel: 4000},
   ]);
 
-  // con computed + css en el componente html
-  // powerClass = computed(() => {
-  //   return {
-  //     'high-power': this.characters().some(c => c.powerLevel > 9000),
-  //     'medium-power': this.characters().some(c => c.powerLevel > 8000 && c.powerLevel <= 9000),
-  //     'low-power': this.characters().every(c => c.powerLevel <= 8000)
-  //   }
-  // });
-
   // con función + bootstrap
   getBadgeClass(character: Character) {
     if (character.powerLevel > 8000) {
@@ -46,5 +40,30 @@ export class DragonballComponent {
       return 'bg-warning';  // verde
     }
   }
+
+  addCharacter() {
+    if (!this.name() || !this.powerLevelValid()) {
+      return;
+    }
+    console.log('Agregando personaje:', this.name(), this.powerLevelValid());
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      powerLevel: this.powerLevelValid()
+    }
+    this.characters.update(characters => [...characters, newCharacter]);
+
+    this.resetInputs();
+  }
+
+  resetInputs() {
+    this.name.set('');
+    this.powerLevel.set(0);
+  }
+
+  //evitar que el front ingrese powerLevel NaN por input
+  powerLevelValid = computed(() => isNaN(this.powerLevel()) ? 0 : this.powerLevel());
+
 
 }
